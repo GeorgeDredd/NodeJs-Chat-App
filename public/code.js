2204f0c73787e16898c9f77fd884be51ea012849
GeorgeDredd <edozygeorge@gmailcom>
2024-01-19 16:27:50 +0100

    updates

--- public/code.js
+++ public/code.js
@@ -31,6 +31,19 @@
         app.querySelector(".chat-screen #message-input").value = ""
     });
 
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
+        socket.emit("exituser", uname);
+        window.location.href = window.location.href;
+    });
+
+    socket.on("update", function(update){
+        renderMessage("update", update);
+    });
+
+    socket.on("chat", function(message){
+        renderMessage("other", message);
+    });
+
     function renderMessage(type, message){
         let messageContainer = app.querySelector(".chat-screen .messages");
         if(type == "my") {
@@ -43,7 +56,7 @@
                 </div>
             `;
             messageContainer.appendChild(el);
-        } else if(type="other") {
+        } else if(type == "other") {
             let el = document.createElement("div");
             el.setAttribute("class", "message other-message");
             el.innerHTML = `
@@ -53,7 +66,7 @@
                 </div>
             `;
             messageContainer.appendChild(el);
-        } else if(type="update") {
+        } else if(type == "update") {
             let el = document.createElement("div");
             el.setAttribute("class", "update");
             el.innerText = message;
